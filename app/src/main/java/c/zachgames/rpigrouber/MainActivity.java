3e1d96ff3ecec9f928854e42930ba080388a4e0e
chaud <dchau2017@gmail.com>
2019-01-25 19:22:38 -0500

    Added barebones login page

--- app/src/main/java/c/zachgames/rpigrouber/MainActivity.java
+++ app/src/main/java/c/zachgames/rpigrouber/MainActivity.java
@@ -2,13 +2,63 @@ package c.zachgames.rpigrouber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
+    private int incorrect = 5;
+    //private TextView title;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //afEAg
+
+        Email = (EditText)findViewById(R.id.etEmail);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Login = (Button)findViewById(R.id.btnLogin);
+        //title.setText("RPI GroUber");
+
+
+
+        Login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                //Converts user input email and password into strings, then checks if user and password are correct
+                validate(Email.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate(String userEmail, String userPassword)
+    {
+        if( (userEmail.equals("Admin")) && (userPassword.equals("yeet")) )
+        {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);        //Go to some second activity
+            startActivity(intent);      //Move to next page
+        }
+        else        //IF password is incorrect
+        {
+            incorrect--;
+            if(incorrect == 0)
+            {
+                Login.setEnabled(false);        //Disable login after 5 tries
+            }
+        }
+
     }
 }
+
+
