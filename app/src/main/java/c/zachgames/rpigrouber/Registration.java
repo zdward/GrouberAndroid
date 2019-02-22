efdf7f7162e7e6a2ab2e904b5fd6eb2724adcfe8
johnhong36 <johnjohn36hong@gmail.com>
2019-02-22 14:57:09 -0500

    Added a registration page that takes in the information of a user.

--- app/src/main/java/c/zachgames/rpigrouber/Registration.java
+++ app/src/main/java/c/zachgames/rpigrouber/Registration.java
@@ -2,12 +2,39 @@ package c.zachgames.rpigrouber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class Registration extends AppCompatActivity{
+
+    private Button Create;
+    private EditText password;
+    private EditText con_password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
+
+        Create = (Button)findViewById(R.id.btnCreate);
+        password = (EditText)findViewById(R.id.password);
+        con_password = (EditText)findViewById(R.id.confirmPassword);
+
+
+        //Create Button
+        Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //goes to the registration page
+                if(password.getText().toString().equals(con_password.getText().toString())) {
+                    Intent intent2 = new Intent(c.zachgames.rpigrouber.Registration.this, Login.class);
+                    startActivity(intent2);
+                }
+            }
+        });
+
     }
 }
